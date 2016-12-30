76f8f85ace56f52ce75dd3f85fb5e563da1c0cbd
martin-honnen <martin.honnen@gmx.de>
2016-12-30 12:34:25 +0100

    HTML/Javascript sample for XSLT transformation with XML and XSLT as string

---
+++ xslt/2016/test2016123001.js
@@ -0,0 +1,30 @@
+var inputXml = [
+	'<products>',
+	'  <test>',
+    '    <id>test_value1</id>',
+    '    <value>1</value>',
+    '  </test>',
+    '  <test>',
+    '    <id>test_value2</id>',
+    '    <value>2</value>',
+    '  </test>',
+    '</products>'
+].join('\n');
+
+var xsltCode = [
+	'<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+
+    '<!-- pass in a string with a single id value for the parameters ids or a "|" separated list of ids -->',
+    '<xsl:param name="ids" select="\'\'"/>',
+    
+    '<xsl:output method="html" indent="yes"/>',
+    
+    '<xsl:template match="/">',
+
+    '    <xsl:for-each select="products/test[$ids = \'\' or contains(concat(\'|\', $ids, \'|\'), id)]">',
+    '        <a href="#"> Hello # <xsl:value-of select="value"/></a>'.
+    '    </xsl:for-each>',
+
+    '</xsl:template>',
+    '</xsl:stylesheet>'
+].join('\n');
