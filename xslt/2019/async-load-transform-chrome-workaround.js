f504e28b594de59f7c95af65744909d1eaad8482
martin-honnen <martin-honnen@users.noreply.github.com>
2019-06-29 20:28:00 +0200

    cloning result of transformToFragment to have Webkit/Chromium mark script for execution
    Trying workaround for Webkit/Chromium bug https://bugs.chromium.org/p/chromium/issues/detail?id=266305 where script elements in XSLTProcessor's transformToFragment result are not executed when inserted into DOM tree although https://www.w3.org/TR/html52/semantics-scripting.html#interaction-of-script-elements-and-xslt demands that.

---
+++ xslt/2019/async-load-transform-chrome-workaround.js
@@ -0,0 +1,54 @@
+      function loadDoc(url) {
+           return new Promise(function(resolve) {
+             var req = new XMLHttpRequest();
+             req.open("GET", url);
+             if (typeof XSLTProcessor === 'undefined') {
+               try {
+                 req.responseType = 'msxml-document';
+               }
+               catch (e) {}
+             }
+             req.onload = function() {
+               resolve(this.responseXML)
+             }
+             req.send();
+           });
+      }
+      
+      function transform(xmlUrl, xslUrl, xsltParams, targetElement) {
+        Promise.all([loadDoc(xmlUrl), loadDoc(xslUrl)]).then(function(data) {
+          var xmlDoc = data[0];
+          var xslDoc = data[1];
+          
+          if (typeof XSLTProcessor !== 'undefined') {
+            var proc = new XSLTProcessor();
+            proc.importStylesheet(xslDoc);
+            
+            for (var prop in xsltParams) {
+              proc.setParameter(null, prop, xsltParams[prop]);
+            }
+            
+            var resultFrag = targetElement.ownerDocument.importNode(proc.transformToFragment(xmlDoc, targetElement.ownerDocument), true); // doesn't work with Webkit as script code in XSLT result fragment is not executed on appendChild: proc.transformToFragment(xmlDoc, targetElement.ownerDocument);
+            
+            targetElement.textContent = '';
+            targetElement.appendChild(resultFrag);
+          }
+          else {
+              var template = new ActiveXObject('Msxml2.XslTemplate.6.0');
+              template.stylesheet = xslDoc;
+              var proc = template.createProcessor();
+              
+              for (var prop in xsltParams) {
+                proc.addParameter(prop, xsltParams[prop]);
+              }
+              
+              proc.input = xmlDoc;
+              
+              proc.transform();
+              
+              var resultHTML = proc.output;
+              
+              targetElement.innerHTML = resultHTML;
+          }
+        });
+      }
