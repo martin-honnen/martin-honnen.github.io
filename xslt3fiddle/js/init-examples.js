b7ba806ab86d432613cc68965ea8a3962bb14147
Martin Honnen <martin.honnen@gmx.de>
2020-06-27 18:22:24 +0200

    saving input and XSLT in URL query string

--- xslt3fiddle/js/init-examples.js
+++ xslt3fiddle/js/init-examples.js
@@ -7,6 +7,43 @@ function setEditorFromUrl(url, editor) {
     req.send();
 }
 
-setEditorFromUrl('examples/defaults/default.xml', inputEditor);
-setEditorFromUrl('examples/defaults/default.xsl', xsltEditor);
+function loadDefaults() {
+  setEditorFromUrl('examples/defaults/default.xml', inputEditor);
+  setEditorFromUrl('examples/defaults/default.xsl', xsltEditor);
+}
+
+
+function save(form) {
+  history.pushState(null, null, '?' + new URLSearchParams(
+    {
+      xslt: xsltEditor.session.getValue(),
+      input: inputEditor.session.getValue(),
+      'input-type': form.elements['input-type'].value
+    }
+  ).toString());
+}
+
+function load(location) {
+  if (!location.search) {
+    loadDefaults();
+  }
+  var searchParams = new URL(location).searchParams;
+  if (searchParams.has("input") && searchParams.has("xslt") && searchParams.has("input-type")) {
+    const inputCode = searchParams.get('input');
+    const xsltCode = searchParams.get('xslt');
+    const inputType = searchParams.get('input-type');
+
+    setDocument(xsltEditor, xsltCode, 'xml');
+    setDocument(inputEditor, inputCode, inputType === 'JSON' ? 'json' : 'xml');
+
+    document.getElementById('input-type-form').elements['input-type'].value = inputType;
+  }
+  else {
+    loadDefaults();
+  }
+}
+
+load(document.location);
+
+
 
