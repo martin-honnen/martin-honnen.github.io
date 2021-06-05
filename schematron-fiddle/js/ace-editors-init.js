500ac97fcd47838ae07b6652f66a1dbb2009e6bf
Martin Honnen <martin.honnen@gmx.de>
2021-06-05 12:13:50 +0200

    fixing case of element name

--- schematron-fiddle/js/ace-editors-init.js
+++ schematron-fiddle/js/ace-editors-init.js
@@ -21,7 +21,7 @@ schematronEditor.session.setValue(`<schema xmlns="http://purl.oclc.org/dsdl/sche
     <pattern>
         <rule context="root">
             <assert test="@*">root has no attributes.</assert>
-            <report test=". = 'This is an example.'">Root element has value 'This is an example.'</report>
+            <report test=". = 'This is an example.'">root element has value 'This is an example.'</report>
         </rule>
     </pattern>
 </schema`);
