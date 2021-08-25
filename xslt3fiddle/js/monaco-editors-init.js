b0590eebf6da6582593db76deb31fa4605db2d2a
Martin Honnen <martin.honnen@gmx.de>
2021-08-25 18:44:29 +0200

    More changes to get Monaco to run of cdnjs

--- xslt3fiddle/js/monaco-editors-init.js
+++ xslt3fiddle/js/monaco-editors-init.js
@@ -1,17 +1,24 @@
-var inputEditor = monaco.editor.create(document.getElementById('input-editor'), { language: 'xml' });
-//inputEditor.session.setMode('ace/mode/xml');
+var inputEditor, xsltEditor, resultEditor;
 
-//inputEditor.session.setTabSize(2);
-//inputEditor.session.setUseSoftTabs(true);
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.27.0/min/vs' } });
+require(["vs/editor/editor.main"], () => {
 
-var xsltEditor = monaco.editor.create(document.getElementById('xslt-editor'), { language: 'xml' }); //ace.edit('xslt-editor');
-//xsltEditor.session.setMode('ace/mode/xml');
 
-//xsltEditor.session.setTabSize(2);
-//xsltEditor.session.setUseSoftTabs(true);
+  inputEditor = monaco.editor.create(document.getElementById('input-editor'), { language: 'xml' });
+  //inputEditor.session.setMode('ace/mode/xml');
 
-var resultEditor = monaco.editor.create(document.getElementById('result-editor'), { language: 'xml' }); //ace.edit('result-editor');
-//resultEditor.session.setMode('ace/mode/xml');
+  //inputEditor.session.setTabSize(2);
+  //inputEditor.session.setUseSoftTabs(true);
 
-//resultEditor.session.setTabSize(2);
-//resultEditor.session.setUseSoftTabs(true);
+  xsltEditor = monaco.editor.create(document.getElementById('xslt-editor'), { language: 'xml' }); //ace.edit('xslt-editor');
+  //xsltEditor.session.setMode('ace/mode/xml');
+
+  //xsltEditor.session.setTabSize(2);
+  //xsltEditor.session.setUseSoftTabs(true);
+
+  resultEditor = monaco.editor.create(document.getElementById('result-editor'), { language: 'xml' }); //ace.edit('result-editor');
+  //resultEditor.session.setMode('ace/mode/xml');
+
+  //resultEditor.session.setTabSize(2);
+  //resultEditor.session.setUseSoftTabs(true);
+});
