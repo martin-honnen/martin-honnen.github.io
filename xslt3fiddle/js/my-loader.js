c224a2d776c80c46c3d2f8bec2807e08c5833fee
Martin Honnen <martin.honnen@gmx.de>
2021-08-26 11:21:30 +0200

    Fixing URL

--- xslt3fiddle/js/my-loader.js
+++ xslt3fiddle/js/my-loader.js
@@ -1,4 +1,4 @@
 self.MonacoEnvironment = {
   baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.27.0/min/'
 };
-importScripts('cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.27.0/min/vs/base/worker/workerMain.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.27.0/min/vs/base/worker/workerMain.js');
