f145f994f3fcd0fee106f8a404b032e1b9501a1e
martin-honnen <martin-honnen@users.noreply.github.com>
2022-04-21 06:56:19 +0200

    test

---
+++ xslt/2022/example2.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const xslt = `<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform version="3.0" xmlns="http://www.w3.org/1999/xhtml">
+    <xsl:template match="/">
+      <html lang="en>
+        <head>
+          <title>Test</title>
+        </head>
+        <body>
+          <h1>Test</h1>
+          <div><code>{serialize(., map { 'method': 'xml', 'indent' : true() })}</code></div>
+        </body>
+      </html>
+    </xsl:template>
+  </xsl:stylesheet>`;
+  
+  const result = SaxonJS.XPath.evaluate(`
+    transform(
+      'source-node' : .,
+      'stylesheet-text' : $xslt
+    )?output
+  `, document, { params : { xslt : xslt } });
+  
+  document.replaceChildren();
+  document.appendChild(result);
+});
