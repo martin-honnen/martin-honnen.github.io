bcd6783a593bea311c9be36b5a4e132d39063bb3
martin-honnen <martin-honnen@users.noreply.github.com>
2022-04-21 06:59:09 +0200

    fix

--- xslt/2022/example2.js
+++ xslt/2022/example2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const xslt = `<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform version="3.0" xmlns="http://www.w3.org/1999/xhtml">
+  const xslt = `<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform version="3.0" xmlns="http://www.w3.org/1999/xhtml" expand-text="yes>
     <xsl:template match="/">
       <html lang="en>
         <head>
@@ -15,8 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const result = SaxonJS.XPath.evaluate(`
     transform(
-      'source-node' : .,
-      'stylesheet-text' : $xslt
+      map {
+        'source-node' : .,
+        'stylesheet-text' : $xslt
+      }
     )?output
   `, document, { params : { xslt : xslt } });
   
