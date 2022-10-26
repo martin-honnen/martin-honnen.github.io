ec38aaba3ecd377d9d2c51fa50aa3317c22212c9
Martin Honnen <martin.honnen@gmx.de>
2022-10-26 12:02:11 +0200

    First stab at getting parameter from HTML form and JavaScript to XPath call of fn:transform and then to stylesheet-params parameter of that function

--- xslt3fiddle/js/parameterHandling.js
+++ xslt3fiddle/js/parameterHandling.js
@@ -6,10 +6,26 @@ function addParameterRow(currentRow) {
   currentRow.parentElement.insertBefore(newRow, currentRow.nextSibling);
 }
 
+var xsltParams = new SaxonJS.XdmMap();
 
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-
+    document.getElementById('setParamsBtn').addEventListener('click',
+      () => {
+        xsltParams = new SaxonJS.XdmMap();
+        const paramRows = document.querySelectorAll('#paramsTable tr');
+        for (let row in paramRows) {
+          const fields = row.querySelectorAll('input[type="text"]');
+          const nameField = fields[0];
+          const valueField = fields[1];
+          if (nameField.value != '' && valueField.value != '') {
+            const qname = SaxonJS.XS.QName.fromEQName(nameField.value);
+            const value = [new SaxonJS.atomic(valueField.value)];
+            xsltParams.inSituPut(qname, value);
+          }
+        }
+      }
+    );
   }
 )
