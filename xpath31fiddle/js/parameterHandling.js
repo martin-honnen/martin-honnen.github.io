2f23e66df3433b238c525d039d06eae59b773c8d
Martin Honnen <martin.honnen@gmx.de>
2022-10-28 12:20:01 +0200

    adding parameter/value dialog to XPath 3.1 fiddle

---
+++ xpath31fiddle/js/parameterHandling.js
@@ -0,0 +1,34 @@
+function addParameterRow(currentRow) {
+  const newRow = currentRow.cloneNode(true);
+  newRow.querySelectorAll('input[type="text"]').forEach(input => {
+    input.defaultValue = input.value = '';
+  });
+  currentRow.parentElement.insertBefore(newRow, currentRow.nextSibling);
+}
+
+var xpathParams = {};
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    document.getElementById('setParamsBtn').addEventListener('click',
+      () => {
+        xpathParams = {};
+        const paramRows = document.querySelector('#params-table').tBodies[0].rows;
+        for (let i = 0; i < paramRows.length; i++) {
+          const row = paramRows[i];
+          const fields = row.querySelectorAll('input[type="text"]');
+          const nameField = fields[0];
+          const valueField = fields[1];
+          if (nameField.value != '' && valueField.value != '') {
+            //const qname = SaxonJS.XS.QName.fromEQName(nameField.value);
+            const type = row.querySelector('select').value;
+            const isXPathExpression = row.querySelector('input[type="checkbox"]').checked;
+            const value = !isXPathExpression ? SaxonJS.atom(valueField.value, type) : SaxonJS.XPath.evaluate(valueField.value, null, { resultForm: 'xdm' });
+            xpathParams[nameField.value] = value;
+          }
+        }
+      }
+    );
+  }
+)
