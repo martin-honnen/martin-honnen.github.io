a8e18542e32850a11c75a10c5c37b11c5ca9958e
Martin Honnen <martin.honnen@gmx.de>
2022-11-29 13:39:39 +0100

    New update of Ace9 editor component

--- xslt3fiddle/js/ace-editors-init.js
+++ xslt3fiddle/js/ace-editors-init.js
@@ -1,11 +1,13 @@
-﻿var inputEditor = ace.edit('input-editor');
+var inputEditor = ace.edit('input-editor');
 inputEditor.session.setMode('ace/mode/xml');
+inputEditor.setOptions({ enableBasicAutocompletion: true });
 
 inputEditor.session.setTabSize(2);
 inputEditor.session.setUseSoftTabs(true);
 
 var xsltEditor = ace.edit('xslt-editor');
 xsltEditor.session.setMode('ace/mode/xml');
+xsltEditor.setOptions({ enableBasicAutocompletion: true });
 
 xsltEditor.session.setTabSize(2);
 xsltEditor.session.setUseSoftTabs(true);
