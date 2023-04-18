8e837d5ccb84125981eb9fc524d6cfd2b79c8f4f
martin-honnen <martin-honnen@users.noreply.github.com>
2023-04-18 17:42:35 +0200

    Update validate.js

--- xml-workbench/js/validate.js
+++ xml-workbench/js/validate.js
@@ -8,7 +8,7 @@ from xmlschema import XMLSchema, XMLSchema11, iter_errors
 schema_class = XMLSchema11
 errors = list(iter_errors(js.xml, js.xsd, cls=schema_class))
 result = 'XML is valid.' if not errors else 'XML is invalid:\\n' + '\\n'.join(map(lambda e: e.reason, errors))
-js.setDocument(resultEditor, result, 'text');
+js.setDocument(js.resultEditor, result, 'text');
 js.console.log(result)
   `);
 }
