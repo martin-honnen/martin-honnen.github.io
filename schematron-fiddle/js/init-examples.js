756ec67622ec815e8d7cf144a2a44281c7a42016
martin-honnen <martin-honnen@users.noreply.github.com>
2023-11-23 10:28:10 +0100

    Create init-examples.js

---
+++ schematron-fiddle/js/init-examples.js
@@ -0,0 +1,48 @@
+function setEditorFromUrl(url, editor) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function () {
+        setDocument(editor, req.responseText, 'xml');
+    };
+    req.send();
+}
+
+function loadDefaults() {
+    //setEditorFromUrl('examples/defaults/default.xml', inputEditor);
+    //setEditorFromUrl('examples/defaults/default.xsl', codeEditor);
+}
+
+
+function save(form) {
+    history.pushState(null, null, '?' + new URLSearchParams(
+        {
+            code: codeEditor.session.getValue(),
+            input: inputEditor.session.getValue(),
+            schxsltVersion: document.getElementById('schxsltVersionSelect').value
+        }
+    ).toString());
+}
+
+function load(location) {
+    if (!location.search) {
+        loadDefaults();
+    }
+    var searchParams = new URL(location).searchParams;
+    if (searchParams.has("input") && searchParams.has("code"))) {
+        const inputCode = searchParams.get('input');
+        const code = searchParams.get('code');
+        const schxsltVersion = searchParams.get('schxsltVersion');
+
+        setDocument(codeEditor, code, 'xml');
+        setDocument(inputEditor, inputCode, 'xml');
+
+        document.getElementById('schxsltVersionSelect').value = schxsltVersion;
+
+        document.getElementById('input-type-form').elements['input-type'].value = inputType;
+    }
+    else {
+        loadDefaults();
+    }
+}
+
+load(document.location);
