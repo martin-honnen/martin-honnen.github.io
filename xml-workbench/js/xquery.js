15be94d3195ddf770571b19a2d9a4effb9cc5846
martin-honnen <martin-honnen@users.noreply.github.com>
2023-12-04 14:23:00 +0100

    Update xquery.js

--- xml-workbench/js/xquery.js
+++ xml-workbench/js/xquery.js
@@ -9,7 +9,7 @@ function xquery(input, xquery, inputType, resultsSelect) {
             body: JSON.stringify({
                 inputCode: xquery,
                 inputData: input,
-                inputType: inputType == 'XML' ? 0 : inputType == 'JSON' ? 1 : 2
+                inputType: inputType == 'XML' ? 0 : inputType == 'JSON' ? 1 : inputType == 'TEXT' ? 2 : 3
             })
         })
         .then(response => response.json()
