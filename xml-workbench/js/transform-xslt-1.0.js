9306880402adf9b64c255033269f11ca2f46352c
martin-honnen <martin-honnen@users.noreply.github.com>
2024-05-31 10:25:32 +0200

    Create transform-xslt-1.0.js

---
+++ xml-workbench/js/transform-xslt-1.0.js
@@ -0,0 +1,22 @@
+function transform_xslt_1(xml, xslt, resultsSelect) {
+  let localsMap = new Map();
+  localsMap.set('xml', xml);
+  localsMap.set('xslt', xslt);
+  
+  pyodide.runPython(`
+import js
+import lxml
+from lxml import etree as ET
+xml_tree = ET.fromstring(xml)
+xslt_tree = ET.fromstring(xslt)
+xslt_transformer = ET.XSLT(xslt_tree)
+transformation_result = xslt_transformer(xml_tree)
+result = str(transformation_result)
+js.setDocument(js.resultEditor, result, 'text');
+js.console.log(result)
+  `,
+  {
+    globals: pyodide.globals,
+    locals: localsMap
+  });
+}
