d420bee659d42321e420309bdc5362cf72bab64c
martin-honnen <martin-honnen@users.noreply.github.com>
2024-07-26 09:59:08 +0200

    Update validate.js

--- schematron-fiddle/js/validate.js
+++ schematron-fiddle/js/validate.js
@@ -3,7 +3,7 @@ const doneTypingInterval = 1000;
 var autoValidate = false;
 
 const schxsltVersions = {
-  'schxslt-1.9.5' : 'schxslt/schxslt-1.19/run-pipeline-for-svrl-and-apply-to-schema.xsl.sef.json',
+  'schxslt-1.10' : 'schxslt/schxslt-1.10/run-pipeline-for-svrl-and-apply-to-schema.xsl.sef.json',
   'schxslt-1.9.5' : 'schxslt/schxslt-1.9.5/run-pipeline-for-svrl-and-apply-to-schema.xsl.sef.json',
   'schxslt2-1.1' : 'schxslt/schxslt2-1.1/run-pipeline-for-svrl-and-apply-to-schema.xsl.sef.json',
   'schxslt2-1.0' : 'schxslt/schxslt2-1.0/run-pipeline-for-svrl-and-apply-to-schema.xsl.sef.json'
