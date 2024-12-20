0a36d00b72b7cdc5b4a1dd8018ea0209f981e479
martin-honnen <martin-honnen@users.noreply.github.com>
2024-12-20 18:01:45 +0100

    Update SaxonHECheerpLatestWorker.js

--- cheerpj3-saxonhe12-fiddle/js/SaxonHECheerpLatestWorker.js
+++ cheerpj3-saxonhe12-fiddle/js/SaxonHECheerpLatestWorker.js
@@ -112,12 +112,12 @@ onmessage = async (e) => {
   var task = e.data.task;
   var data = e.data.data;
   if (task === 'transform') {
-    await transform(data.input, data.code, data.inputType, codeBaseURI, inputBaseURI);
+    await transform(data.input, data.code, data.inputType, data.codeBaseURI, data.inputBaseURI);
   }
   else if (task === 'xquery') {
-	  await xquery(data.input, data.code, data.inputType, codeBaseURI, inputBaseURI);  
+	  await xquery(data.input, data.code, data.inputType, data.codeBaseURI, data.inputBaseURI);  
   }
   else if (task === 'xpath') {
-	  await xpath(data.input, data.code, data.inputType, codeBaseURI, inputBaseURI);  
+	  await xpath(data.input, data.code, data.inputType, data.codeBaseURI, data.inputBaseURI);  
   }
 }
