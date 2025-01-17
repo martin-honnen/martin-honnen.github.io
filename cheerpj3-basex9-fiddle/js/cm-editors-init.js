54500ce1b857a38bc02bce7ef3d5a812c0df76aa
martin-honnen <martin-honnen@users.noreply.github.com>
2025-01-17 16:54:56 +0100

    Update cm-editors-init.js

--- cheerpj3-basex9-fiddle/js/cm-editors-init.js
+++ cheerpj3-basex9-fiddle/js/cm-editors-init.js
@@ -1,217 +1,39 @@
-if (document.getElementById('inputEditor')) {
-  // Prefix use in button ID naming
-// Default : CMEditorPrefix
-// More explanation on Buttons Section
-var EditorPrefixInputEditor = 'CMEditorPrefix';
-// EditorSetting[ParentName]
-var EditorSettingInputEditor = {
-'inputEditor' : {
-    // Global var to save editor doc into, if updatelistener is true
-    // In wrapper will be : window[EditorSetting['editor']['data']]
-    // Default : ParentName + "data"
-    data: "inputEditorDoc",
-    // Editor doc update listener, in miliseconds
-    // Default : false
-    updatelistener: 300,
-    // Delay to update history button (Undo Redo), in miliseconds
-    // Default : 10
-    historydelay: 50,
-    // Language of editor : text , html , javascript , css , json , php
-    // Default : text (no language)
-    lang: 'xml',
-    // Snippets to be used in editor
-    // Array : ['xml', 'javascript', 'jquery', 'php']
-    snippets: ['xml', 'json'],
-    // Default : DefaultTheme (Ace Cobalt)
-    theme: 'DefaultTheme',
-    // Enable extension
-    extension: [ "cm6-texttolink" ],
-    // IndenUnit default : 2
-    indentunit: 2,
-    // linewrap default : false
-    linewrap: true,
-    // lineNumbers default : true
-    lineNumbers: true,
-    // Default line where the cursor would be on editor load
-    defaultline: 12,
-    // readonly default : false
-    readonly: 0,
-    // KeyMap to be used : 'indentWithTab' , 'searchKeymap' , 'completionKeymap'
-    keymap: ['indentWithTab', 'searchKeymap', 'completionKeymap'],
-    control: {
-	// Control button on panel
-	// Default : False
-	// panel: 'top' => Panel on top position of editor
-	// panel: true => Panel on bottom position of editor
-	// Show / hide panel button (Not the panel itself)
-	// Default : true
-	// List of buttons to add
+if (document.getElementById('input-editor')) {
+  var inputEditor = CodeMirror(
+    document.getElementById('input-editor'),
+    { mode : 'xml' }
+  );
+    ;
+//inputEditor.session.setMode('ace/mode/xml');
 
-	// Control button prepended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-
-	// Control button appended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-
-	// Control button added into certain element
-	// Element will be emptied first => element.html('')
-	// Default : False
-    },
-    // Add CSS Style into CodeMirror
-    // Read more on https://codemirror.net/examples/styling/
-}
-};
-
-$(document).ready(function() {
-    // Editor init
-    $("#CMEditorPrefixInputEditor-command").val('neweditor-inputEditor').click();
-});
-}
-
-
-if (document.getElementById('codeEditor')) {
-// Prefix use in button ID naming
-// Default : CMEditorPrefix
-// More explanation on Buttons Section
-var EditorPrefixCodeEditor = 'CMEditorPrefix';
-// EditorSetting[ParentName]
-var EditorSettingCodeEditor = {
-'codeEditor' : {
-    // Global var to save editor doc into, if updatelistener is true
-    // In wrapper will be : window[EditorSetting['editor']['data']]
-    // Default : ParentName + "data"
-    data: "codeEditorDoc",
-    // Editor doc update listener, in miliseconds
-    // Default : false
-    updatelistener: 300,
-    // Delay to update history button (Undo Redo), in miliseconds
-    // Default : 10
-    historydelay: 50,
-    // Language of editor : text , html , javascript , css , json , php
-    // Default : text (no language)
-    lang: 'xquery',
-    // Snippets to be used in editor
-    // Array : ['html', 'javascript', 'jquery', 'php']
-    snippets: ['xquery', 'xml'],
-    // Default : DefaultTheme (Ace Cobalt)
-    theme: 'DefaultTheme',
-    // Enable extension
-    extension: [ "cm6-texttolink" ],
-    // IndenUnit default : 2
-    indentunit: 2,
-    // linewrap default : false
-    linewrap: true,
-    // lineNumbers default : true
-    lineNumbers: true,
-    // Default line where the cursor would be on editor load
-    defaultline: 12,
-    // readonly default : false
-    readonly: 0,
-    // KeyMap to be used : 'indentWithTab' , 'searchKeymap' , 'completionKeymap'
-    keymap: ['indentWithTab', 'searchKeymap', 'completionKeymap'],
-    control: {
-	// Control button on panel
-	// Default : False
-	// panel: 'top' => Panel on top position of editor
-	// panel: true => Panel on bottom position of editor
-        panel: 1,
-	// Show / hide panel button (Not the panel itself)
-	// Default : true
-	panelshow: false,
-	// List of buttons to add
-        // 'history' , 'linewrap' , 'readonly' , 'indent' , 'selectall' , 'search' , 'lang' , 'theme'
-	// Control button prepended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-	// Control button appended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-	// Control button added into certain element
-	// Element will be emptied first => element.html('')
-	// Default : False
-    },
-    // Add CSS Style into CodeMirror
-    // Read more on https://codemirror.net/examples/styling/
-}
-};
-
-$(document).ready(function() {
-    // Editor init
-    $("#CMEditorPrefixCodeEditor-command").val('neweditor-codeEditor').click();
-});
-}
-
-// Prefix use in button ID naming
-// Default : CMEditorPrefix
-// More explanation on Buttons Section
-var EditorPrefixResultEditor = 'CMEditorPrefix';
-// EditorSetting[ParentName]
-var EditorSettingResultEditor = {
-'resultEditor' : {
-    // Global var to save editor doc into, if updatelistener is true
-    // In wrapper will be : window[EditorSetting['editor']['data']]
-    // Default : ParentName + "data"
-    data: "resultEditorDoc",
-    // Editor doc update listener, in miliseconds
-    // Default : false
-    updatelistener: 300,
-    // Delay to update history button (Undo Redo), in miliseconds
-    // Default : 10
-    historydelay: 50,
-    // Language of editor : text , html , javascript , css , json , php
-    // Default : text (no language)
-    lang: 'json',
-    // Snippets to be used in editor
-    // Array : ['html', 'javascript', 'jquery', 'php']
-    snippets: ['html', 'xml', 'text'],
-    // Default : DefaultTheme (Ace Cobalt)
-    theme: 'DefaultTheme',
-    // Enable extension
-    extension: [ "cm6-texttolink" ],
-    // IndenUnit default : 2
-    indentunit: 2,
-    // linewrap default : false
-    linewrap: true,
-    // lineNumbers default : true
-    lineNumbers: true,
-    // Default line where the cursor would be on editor load
-    defaultline: 12,
-    // readonly default : false
-    readonly: 0,
-    // KeyMap to be used : 'indentWithTab' , 'searchKeymap' , 'completionKeymap'
-    keymap: ['indentWithTab', 'searchKeymap', 'completionKeymap'],
-    control: {
-	// Control button on panel
-	// Default : False
-	// panel: 'top' => Panel on top position of editor
-	// panel: true => Panel on bottom position of editor
-	// Show / hide panel button (Not the panel itself)
-	// Default : true
-	// List of buttons to add
-        // 'history' , 'linewrap' , 'readonly' , 'indent' , 'selectall' , 'search' , 'lang' , 'theme'
-	// Control button prepended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-
-	// Control button appended into parent element
-	// Default : False
-	// Also as an amount of newline in between
-
-	// Control button added into certain element
-	// Element will be emptied first => element.html('')
-	// Default : False
-
-    },
-    // Add CSS Style into CodeMirror
-    // Read more on https://codemirror.net/examples/styling/
+//inputEditor.session.setTabSize(2);
+//inputEditor.session.setUseSoftTabs(true);
+//inputEditor.setOption('placeholder', 'Enter your XML or JSON input data here...');
+//inputEditor.session.on('change', autoEvaluate);
 
 }
-};
 
-$(document).ready(function() {
-    // Editor init
-    $("#CMEditorPrefixResultEditor-command").val('neweditor-resultEditor').click();
-});
 
+if (document.getElementById('code-editor')) {
+var codeEditor = CodeMirror(
+  document.getElementById('code-editor'),
+  { mode : 'xquery' }
+);
+//codeEditor.session.setMode('ace/mode/xquery');
+
+//codeEditor.session.setTabSize(2);
+//codeEditor.session.setUseSoftTabs(true);
+//codeEditor.setOption('placeholder', 'Enter your XQuery code here...');
+//codeEditor.session.on('change', autoEvaluate);
+
+}
+
+var resultEditor = CodeMirror(
+  document.getElementById('result-editor'),
+  { mode : 'xml' }
+);
+//resultEditor.session.setMode('ace/mode/xml');
+
+//resultEditor.session.setTabSize(2);
+//resultEditor.session.setUseSoftTabs(true);
+//resultEditor.setOption('placeholder', 'Results will appear here...');
