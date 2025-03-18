a3de6b7e97816abacd180e7e485052657be73200
Martin Honnen <martin.honnen@gmx.de>
2025-03-18 16:59:16 +0100

    Initial commit

---
+++ cheerpj-basex-test/js/CheerpJWorkerTest1.js
@@ -0,0 +1,14 @@
+importScripts("https://cjrtnc.leaningtech.com/3_20250318_631/cj3loader.js");
+
+var lib = null;
+
+(async () => {
+  await cheerpjInit({version: 11});
+
+  lib = await cheerpjRunLibrary("");
+
+  console.log('Worker CheerpJ library initialized');
+
+  const System = await lib.java.lang.System;
+  await System.out.println("Hello from Java");
+})();
