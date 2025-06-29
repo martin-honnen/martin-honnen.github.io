f40fef511e124d235f1b659cf5dcfa188fed9a66
Martin Honnen <martin.honnen@gmx.de>
2025-06-29 16:02:16 +0200

    Fix code

--- schematron-fiddle/js/validate-cheerpj-saxonjhe.js
+++ schematron-fiddle/js/validate-cheerpj-saxonjhe.js
@@ -1,9 +1,9 @@
-function validate(xml, schematron, resultsSelect, schxltVersion) {
+function validate(xml, schematron, schxsltVersion) {
 
   if (!autoEvaluation)
     setDocument(resultEditor, "Processing your Schematron...", "text");
 
-  saxonWorker.postMessage({ task: 'schematron', data : { input : xml, schematron: schematron, schxltVersion: schxltVersion } });
+  saxonWorker.postMessage({ task: 'schematron', data : { input : xml, schematron: schematron, schxsltVersion: schxsltVersion } });
 
 }
 
