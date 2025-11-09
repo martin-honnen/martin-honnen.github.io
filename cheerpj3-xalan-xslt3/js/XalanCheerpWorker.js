d1e66b4a878a379b0079cedec7dacd3a7e782339
Martin Honnen <martin.honnen@gmx.de>
2025-11-09 17:40:30 +0100

    update to CheerpJ 4.2 as CheerpJ team says it much improved compared to CheerpJ 3.1, even if running Java 8 code

--- cheerpj3-xalan-xslt3/js/XalanCheerpWorker.js
+++ cheerpj3-xalan-xslt3/js/XalanCheerpWorker.js
@@ -1,4 +1,4 @@
-importScripts("https://cjrtnc.leaningtech.com/3.1/cj3loader.js"); //importScripts("https://cjrtnc.leaningtech.com/3.0/cj3loader.js");//importScripts('https://cjrtnc.leaningtech.com/3_20231211_315/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3_20231211_314/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3_20231202_306/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3.0rc2/cj3loader.js');
+importScripts("https://cjrtnc.leaningtech.com/4.2/loader.js");//importScripts("https://cjrtnc.leaningtech.com/3.1/cj3loader.js"); //importScripts("https://cjrtnc.leaningtech.com/3.0/cj3loader.js");//importScripts('https://cjrtnc.leaningtech.com/3_20231211_315/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3_20231211_314/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3_20231202_306/cj3loader.js'); //'https://cjrtnc.leaningtech.com/3.0rc2/cj3loader.js');
 
 var filetypes = {
   '.htm': 'html',
@@ -56,7 +56,7 @@ var xalanInitialized = false;
   //lib = await cheerpjRunLibrary("/app/xalan.jar:/app/serializer.jar:/app/CheerpJaxpHelpers.jar:/app/lib/bcel-6.7.0.jar:/app/lib/brazil-2.1.jar:/app/lib/bsf.jar:/app/lib/commons-logging-1.2.jar:/app/lib/javaee-api-5.0-2.jar:/app/lib/javaee-api-5.0-2.jar:/app/lib/regexp.jar:/app/lib/rhino-1.7.14.jar:/app/lib/runtime.jar:/app/lib/servlet-api-2.5.jar:/app/lib/xpath31_types.jar:/app/lib/endorsed/xercesImpl.jar:/app/lib/endorsed/xml-apis.jar");
   lib = await cheerpjRunLibrary("/app/cheerpj3-xalan-xslt3/xalan-java-3.0.1-SNAPSHOT.jar:/app/cheerpj3-xalan-xslt3/CheerpJaxpHelpers.jar:/app/cheerpj3-xalan-xslt3/lib/json-20240303.jar:/app/cheerpj3-xalan-xslt3/lib/rhino-1.7.14.jar:/app/cheerpj3-xalan-xslt3/lib/bcel-6.7.0.jar:/app/cheerpj3-xalan-xslt3/lib/commons-logging-1.2.jar:/app/cheerpj3-xalan-xslt3/lib/xpath31_types-1.0.jar:/app/cheerpj3-xalan-xslt3/lib/bsf-1.0.jar:/app/cheerpj3-xalan-xslt3/lib/jlex-1.0.jar:/app/cheerpj3-xalan-xslt3/lib/java_cup-1.0.jar:/app/cheerpj3-xalan-xslt3/lib/cup_runtime-1.0.jar:/app/cheerpj3-xalan-xslt3/lib/xercesImpl-2.12.3.jar:/app/cheerpj3-xalan-xslt3/lib/xml-apis-1.4.02.jar");
 
-  console.log('Worker CheerpJ 3 library initialized');
+  console.log('Worker CheerpJ library initialized');
   
   postMessage({ type: 'message', message : 'hide', id : 'cheerpj3-load-indicator' });
 
