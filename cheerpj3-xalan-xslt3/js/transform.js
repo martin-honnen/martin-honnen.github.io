788b9db378d9ed1db55ad220c13dc8756eb9acf4
martin-honnen <martin-honnen@users.noreply.github.com>
2026-04-02 02:03:08 +0200

    Replace phoenixmlWorker with saxonWorker for transforms

--- cheerpj3-xalan-xslt3/js/transform.js
+++ cheerpj3-xalan-xslt3/js/transform.js
@@ -3,7 +3,7 @@ function transform(input, xslt, inputType, resultsSelect) {
   if (!autoEvaluation)
 	  setDocument(resultEditor, "Processing your XSLT...", "text");
 
-  phoenixmlWorker.postMessage({ task: 'transform', data : { input : input, code: xslt, inputType: inputType }});
+  saxonWorker.postMessage({ task: 'transform', data : { input : input, code: xslt, inputType: inputType }});
 /*   if (saxonInitialized) {
 	  try {
 		  var contextItem = null;
@@ -96,5 +96,5 @@ function transformUrls(inputUrl, xsltUrl, inputType, resultsSelect) {
   if (!autoEvaluation)
 	  setDocument(resultEditor, "Processing your XSLT...", "text");
 
-  phoenixmlWorker.postMessage({ task: 'transform-urls', data : { input : inputUrl, code: xsltUrl, inputType: inputType }});
+  saxonWorker.postMessage({ task: 'transform-urls', data : { input : inputUrl, code: xsltUrl, inputType: inputType }});
 }
