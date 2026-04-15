79086edf20f02d733d75eb9b20d5dd75445b4c20
Martin Honnen <martin.honnen@gmx.de>
2026-04-15 23:14:52 +0200

    Schxslt2 update

--- schematron-fiddle/js/validate-cheerpj-saxonjhe.js
+++ schematron-fiddle/js/validate-cheerpj-saxonjhe.js
@@ -3,7 +3,7 @@ function validate(xml, schematron, schxsltVersion) {
   if (!autoEvaluation)
     setDocument(resultEditor, "Processing your Schematron...", "text");
 
-  phoenixmlWorker.postMessage({ task: 'schematron', data : { input : xml, schematron: schematron, schxsltVersion: schxsltVersion } });
+  saxonWorker.postMessage({ task: 'schematron', data : { input : xml, schematron: schematron, schxsltVersion: schxsltVersion } });
 
 }
 
