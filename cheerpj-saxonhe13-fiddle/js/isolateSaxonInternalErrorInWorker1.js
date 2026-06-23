bb6c582274dc2f8ebeab01186b99731c49ada485
Martin Honnen <martin.honnen@gmx.de>
2026-06-23 20:23:30 +0200

    make Saxon HE 13 XQuery test in web worker

--- cheerpj-saxonhe13-fiddle/js/isolateSaxonInternalErrorInWorker1.js
+++ cheerpj-saxonhe13-fiddle/js/isolateSaxonInternalErrorInWorker1.js
@@ -1,116 +1,104 @@
 importScripts("https://cjrtnc.leaningtech.com/4.3/loader.js");//importScripts("https://cjrtnc.leaningtech.com/4.2/loader.js"); //"https://cjrtnc.leaningtech.com/3_20250414_1093/cj3loader.js"); //https://cjrtnc.leaningtech.com/3_20250330_890/cj3loader.js");//https://cjrtnc.leaningtech.com/3.1/cj3loader.js"); //importScripts("https://cjrtnc.leaningtech.com/3_20241216_574/cj3loader.js"); //importScripts("https://cjrtnc.leaningtech.com/3_20241213_572/cj3loader.js"); //importScripts("https://cjrtnc.leaningtech.com/3.0/cj3loader.js");
 
-var lib = null;
+var input = null;
+var xqueryCode = `let $xslt := <xsl:stylesheet version="3.0"
+                exclude-result-prefixes="#all"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:xs="http://www.w3.org/2001/XMLSchema"
+                xmlns:mf="http://example.com/mf">
 
-var SaxonProcessor = null;
+    <xsl:function name="mf:rng" as="xs:double" visibility="public">
+        <xsl:param name="input" as="xs:double?"/>
 
-var SaxonHelpers = null;
+        <xsl:variable name="value" as="xs:double">
+            <xsl:choose>
+                <xsl:when test="empty($input) or $input = 0 or number($input) != number($input)">
+                    <xsl:sequence select="random-number-generator(current-dateTime())?number"/>
+                </xsl:when>
 
-var StringReader = null;
+                <xsl:when test="$input &lt; 0">
+                    <xsl:sequence select="$input * -1"/>
+                </xsl:when>
 
-var StringWriter = null;
+                <xsl:otherwise>
+                    <xsl:sequence select="$input"/>
+                </xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>
 
-var StreamSource = null;
+        <xsl:sequence select="$value"/>
+    </xsl:function>
 
-var URI = null;
+</xsl:stylesheet>
 
-var JException = null;
+for $n in (1 to 3)
+return
+transform(
+    map {
+      'stylesheet-node' : $xslt,
+      'delivery-format' : 'raw',
+      'initial-function' : QName('http://example.com/mf', 'rng'),
+      'function-params' : [()]
+    }
+  )?output`;
 
-var SaxonApiException = null;
+(async function () {
+  var lib = null;
 
-var SaxonLogger = null;
+  var SaxonProcessor = null;
 
-var saxonProcessor = null;
+  var StringReader = null;
 
-var xsltCompiler = null;
+  var StringWriter = null;
 
+  var StreamSource = null;
 
-var docBuilder = null;
+  var URI = null;
 
+  var JException = null;
 
-(async () => {
+  var SaxonApiException = null;
 
-  await cheerpjInit({version: 8});
+  var saxonProcessor = null;
 
-  lib = await cheerpjRunLibrary("/app/cheerpj3-saxonhe12-fiddle/Saxon-HE-12.9.jar:/app/cheerpj3-saxonhe12-fiddle/Cheerp3Helpers.jar");
-  
-  console.log('Worker CheerpJ 4.2 library initialized');
+  var xqueryCompiler = null;
+
+  var xqueryExecutable = null;
+
+  var xqueryEvaluator = null;
+
+  await cheerpjInit({version: 17});
+
+  lib = await cheerpjRunLibrary("/app/cheerpj-saxonhe13-fiddle/saxon-he-13.0.jar:/app/cheerpj-saxonhe13-fiddle/lib/xmlresolver-6.0.23.jar");
 
   JException = await lib.java.lang.Exception;
-  
+
   SaxonProcessor = await lib.net.sf.saxon.s9api.Processor;
-  
-  SaxonHelpers = await lib.net.liberty_development.cheerp3Helpers.SaxonHelpers;
 
   StringReader = await lib.java.io.StringReader;
 
   StringWriter = await lib.java.io.StringWriter;
 
-  StreamSource = await lib.javax.xml.transform.stream.StreamSource;
 
   URI = await lib.java.net.URI;
-  
+
   SaxonApiException = await lib.net.sf.saxon.s9api.SaxonApiException;
 
-  SaxonLogger = await lib.net.sf.saxon.lib.StandardLogger;
-
   saxonProcessor = await new SaxonProcessor(false);
-  
-  await SaxonHelpers.setProcessor(saxonProcessor);
 
-  docBuilder = await saxonProcessor.newDocumentBuilder();
+  xqueryCompiler = await saxonProcessor.newXQueryCompiler();
 
-  xsltCompiler = await saxonProcessor.newXsltCompiler();
+  xqueryExecutable = await xqueryCompiler.compile(xqueryCode);
 
+  xqueryEvaluator = await xqueryExecutable.load();
 
-  console.log('Worker Saxon initialized');
+  xqueryEvaluator.setContextItem(input);
 
-  var input = `<data>200</data>`;
-  var xsltCode = `<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-                version="3.0"
-                xmlns:xs="http://www.w3.org/2001/XMLSchema"
-                xmlns:f="http://example.com/functions"
-                exclude-result-prefixes="#all"
-                expand-text="yes">
+  var result = await xqueryEvaluator.evaluate();
 
-  <xsl:param name="n" as="xs:integer" select="200"/>
+  var resultString = await result.toString();
 
-  <xsl:output method="xml" indent="yes"/>
-
-  <xsl:function name="f:fib" as="xs:integer" cache="yes">
-    <xsl:param name="num" as="xs:integer"/>
-    <xsl:sequence
-      select="if ($num = 0)
-             then 0
-             else if ($num = 1)
-             then 1
-             else f:fib($num - 2) + f:fib($num - 1)"/>
-  </xsl:function>
-
-  <xsl:template name="xsl:initial-template" match="/">
-    <fib n="{$n}">{f:fib($n)}</fib>
-    <xsl:comment>Run with {system-property('xsl:product-name')} {system-property('xsl:product-version')} at {current-dateTime()}</xsl:comment>
-  </xsl:template>
-
-</xsl:stylesheet>`;
-
-  var results = await SaxonHelpers.runXsltTransformation(input, 'XML', 'urn:from-string', xsltCode, 'urn:from-string');
-
-  var resultDocuments = [];
-
-  var size = results.length;
-
-  for (var i = 0; i < size; i++) {
-    var currentResult = results[i];
-    var uri = await currentResult.getUri();
-    var content = await currentResult.getContent();
-    let method = await currentResult.getMethod();
-    //let suffix = uri.replace(/.*(\.[a-z]+)/gi, '$1').toLowerCase();
-    //let method = filetypes[suffix];
-    resultDocuments.push({ uri: uri, content: content, method: method ? method : 'html' });
-  }
-
-  console.log(resultDocuments);
+  console.log(`XQuery result: ${resultString}`);
 
 })();
 
