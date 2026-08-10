ee62bf8587d36149866ed44be0f6627449e85ac7
Martin Honnen <martin.honnen@gmx.de>
2026-08-10 04:32:15 +0200

    try to pass array of arguments correct main call to pass in path to Schematron and XML file in /str file system

--- cheerpj-mausotron/js/schematron-validate-async.js
+++ cheerpj-mausotron/js/schematron-validate-async.js
@@ -5,7 +5,7 @@ async function schematronValidate(input, schematron, inputUri, xsltUri, outputUr
     try {
       cheerpOSAddStringFile("/str/schematron.sch", schematron);
       cheerpOSAddStringFile("/str/sample.xml", input);
-      await mainClass.main("validate", "--verbose", input, schematron);
+      await mainClass.main(["validate", "--verbose", "/str/schematron.sch", "/str/sample.xml"]);
     }
     catch (e) {
       postMessage({ 'type': 'error', message: 'Schematron validation of your XML failed: ' + (typeof e === 'string' ? e : await e.getMessage()) });
